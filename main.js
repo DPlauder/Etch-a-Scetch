5b16404c16771aa6eacdb401a86b4c9ab68c143f
Dominik <docgit@gmail.comgit commit -m first>
2023-06-06 08:49:08 +0200

    schrit 1 abgeschlossen

--- main.js
+++ main.js
@@ -3,13 +3,34 @@ function grid() {
     for (let i = 0; i < 16; i++) {
         for (let j = 0; j < 16; j++) {
             let quadrad = document.createElement('div');
-            quadrad.style.width = '80px';
-            quadrad.style.height= '80px';
+            quadrad.style.width = 800 / 16 + "px";
+            quadrad.style.height= 800 / 16 + "px";
             quadrad.classList.add('quad');
-            feld.appendChild(quadrad)
-            quadrad.style.backgroundColor = 'green'
-            quadrad.style.border = '2px solid'
+            feld.appendChild(quadrad);
+            quadrad.style.border = '1px solid';
         }
     }
+    hovern()
+}
+function hovern() {
+    const felder = document.getElementsByClassName('quad');
+    for (const element of felder) {
+        element.addEventListener('mouseover', () => {
+            const rot = Math.floor(Math.random() * 256);
+            const gruen = Math.floor(Math.random() * 256);
+            const blau = Math.floor(Math.random() * 256);
+            element.style.backgroundColor = `rgb(${rot}, ${gruen}, ${blau})`;
+        })        
+    }
+}
+function reset() {
+    const resetten = document.querySelector('#reset'); 
+    const felder = document.querySelector('#drawpad');
+    resetten.addEventListener('click', () => {
+        felder.innerHTML = "";
+        grid();
+    })
 }
 grid();
+reset()
+
